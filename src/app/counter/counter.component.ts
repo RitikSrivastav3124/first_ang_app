ed97f9885752e1ead883c7edbbf086ffe69d4a12
Ritik <ritik022ai0062@rdec.in>
2026-01-20 09:56:38 +0530

    first

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+protected readonly name = signal('CounterComponent');
+}
